a5bf31ea323541affe1b24a7c78189fa17724e21
John McElmurray <john@outlook.com>
2014-04-15 22:18:37 -0700

    Project file setup
    One project for shared controls
    One project for the application

---
+++ Helios/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Helios
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Navigation;
+    using Microsoft.Phone.Controls;
+    using Microsoft.Phone.Shell;
+    using Helios.Resources;
+
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
