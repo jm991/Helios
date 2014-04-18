c118c68bc0aefd5abfefe0e89390c2176b9bddc3
John McElmurray <john@outlook.com>
2014-04-17 21:32:32 -0700

    Open a video
    The app can now open a video file

--- TestApp/TestApp/MainPage.xaml.cs
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Media.Editing;
 using Windows.Media.Effects;
+using Windows.Storage.Pickers;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,7 +28,40 @@ namespace TestApp
     {
         public MainPage()
         {
-            this.InitializeComponent();            
+            this.InitializeComponent();
+            PickAFileButton.Click += new RoutedEventHandler(PickAFileButton_Click);
+        }
+
+        private void PickAFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.Storage.StorageFile source;
+
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".mp4");
+            openPicker.FileTypeFilter.Add(".wmv");
+            openPicker.FileTypeFilter.Add(".avi");
+
+            // Launch file open picker and caller app is suspended and may be terminated if required
+            openPicker.PickSingleFileAndContinue();
+        }
+
+        /// <summary>
+        /// Handle the returned files from file picker
+        /// This method is triggered by ContinuationManager based on ActivationKind
+        /// </summary>
+        /// <param name="args">File open picker continuation activation argment. It cantains the list of files user selected with file open picker </param>
+        public void ContinueFileOpenPicker(FileOpenPickerContinuationEventArgs args)
+        {
+            if (args.Files.Count > 0)
+            {
+                Debug.Log("Picked video: " + args.Files[0].Name);
+            }
+            else
+            {
+                Debug.Log(OutputTextBlock.Text = "Operation cancelled.");
+            }
         }
 
         /// <summary>
